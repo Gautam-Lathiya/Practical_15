301182ad125889bb602837dd624391799d82460b
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-15 10:51:23 +0530

    Add project files.

---
+++ FormAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using FormAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT Role FROM Users WHERE Username = @Username AND Password = @Password";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Username", model.Username);
+                    cmd.Parameters.AddWithValue("@Password", model.Password);
+
+                    var role = cmd.ExecuteScalar() as string;
+                    if (role != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        Session["Role"] = role;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    Response.Write("Invalid username or password.");
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+
+}
